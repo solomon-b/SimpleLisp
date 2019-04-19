fb1f4713ff51b128275436408cd3c0948f33c141
Solomon Bothwell <ssbothwell@gmail.com>
2019-04-19 13:59:42 -0700

    Another big commit
    Moves Nil from DotList to Term to better reflect Lisp's DotList semantics.
    Adds a LispM type that wraps StateT and ExceptT.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DeriveTraversable #-}
 module Lib where
+
+import Debug.Trace
 
 import Data.Typeable
 import Data.Functor
@@ -19,7 +22,7 @@ import System.Console.Haskeline
 import Text.Trifecta
 import Text.Parser.Combinators
 
-data DotList a = Nil | a :-: (DotList a) | a :-. a
+data DotList a = a :-: (DotList a) | a :-. a
   deriving (Functor, Foldable, Traversable, Eq, Show)
 
 infixr 5 :-:
@@ -42,9 +45,12 @@ data Term
   | Boolean Bool
   | List (DotList Term)
   | DotList (DotList Term)
+  | Nil
   deriving (Eq, Show)
 
-data Arrity = Unary Term | Binary Term Term | Nary (DotList Term)
+data Arrity = Unary Term | Binary Term Term
+
+newtype Env = Env [(String, Term)] deriving Show
 
 --instance Semigroup Term where
 --  (<>) (List ts) (List ts') = List (ts <> ts')
@@ -66,6 +72,7 @@ data EvalError
   = TypeError String Term
   | WrongArrity String Int Int
   | ObjectNotApplicable Term
+  | NotAProperList Term
   | UnspecifiedReturn
   | IllFormedSyntax
   deriving Eq
@@ -76,9 +83,9 @@ instance Show EvalError where
   show (WrongArrity func arrity params) = "The procedure " ++ func ++ " has been called with " ++ show params ++
                                       " arguments; it requires exactly " ++ show arrity ++ " argument"
   show (ObjectNotApplicable obj) = "The object '" ++ show obj ++ "' is not applicable."
+  show (NotAProperList term) = "Combination must be a proper list: " ++ show term
   show UnspecifiedReturn = "Unspecified return value."
   show IllFormedSyntax = "Ill-formed syntax"
-
 
 ----------------
 ---- Parser ----
@@ -98,13 +105,19 @@ parseBool :: Parser Term
 parseBool = Boolean <$> (string "True" $> True <|> (string "False" $> False))
 
 parseQuote :: Parser Term
-parseQuote = (\x -> List (Symbol "quote" :-: x :-: Nil)) <$> (void (char '\'') *> parseTerm)
+parseQuote = (\x -> List (Symbol "quote" :-: x :-. Nil)) <$> (void (char '\'') *> parseTerm)
 
 parseScalars :: Parser Term
 parseScalars = parseQuote <|> parseNumber <|> parseString' <|> parseBool <|> parseSymbol 
 
 parseRegList :: Parser Term
-parseRegList = parens $ List . foldr (:-:) Nil <$> parseTerm `sepBy` spaces
+parseRegList = parens $ do
+  xs <- parseTerm `sepBy` spaces
+  if null xs
+  then return Nil
+  else return . List $ f xs
+  where f [x] = x :-. Nil
+        f (x:xs) = x :-: f xs
 
 parseDotList :: Parser Term
 parseDotList = parens $ do
@@ -136,44 +149,159 @@ parse = parseString parseTerm mempty
 -- lambda
 -- label
 -- fix :D
+-- various arithmetic
+-- various logic
 
 -- | Factor out the primitive function pattern matching into a global context object (ReaderT)
-evalTerm :: MonadError EvalError m => Term -> m Term
-evalTerm (List (Symbol "quote" :-: value :-: Nil)) = return value
+evalTerm :: (MonadState env m, MonadError EvalError m) => Term -> m Term
+evalTerm (List (Symbol "add"   :-. Nil))  = return $ Number 0            -- special case for no arrity
+evalTerm (List (Symbol "cond"  :-. Nil))  = throwError UnspecifiedReturn -- special case for no arrity
+evalTerm (List (Symbol "quote" :-: value :-. Nil)) = return value
 evalTerm (List (Symbol "atom?" :-: args)) = arrity 1 "atom?" atom =<< traverse evalTerm args
 evalTerm (List (Symbol "cons"  :-: args)) = arrity 2 "cons" cons  =<< traverse evalTerm args
 evalTerm (List (Symbol "car"   :-: args)) = arrity 1 "car" car    =<< traverse evalTerm args
 evalTerm (List (Symbol "eq?"   :-: args)) = arrity 2 "eq?" eq     =<< traverse evalTerm args
 evalTerm (List (Symbol "cdr"   :-: args)) = arrity 1 "cdr" cdr    =<< traverse evalTerm args
-evalTerm (List (Symbol "cond"  :-: args)) = cond               =<< traverse (evalTerm <=< quotePredicates) args
-evalTerm (List (Symbol "add"   :-: args)) = Number . sum       <$> traverse (asInteger <=< evalTerm) args
-evalTerm (List Nil)                       = List               <$> traverse evalTerm Nil
-evalTerm (List xs)                        = badAppplication    =<< traverse evalTerm xs
+evalTerm (List (Symbol "cond"  :-: args)) = cond                  =<< traverse (evalTerm <=< quotePredicates) args
+evalTerm (List (Symbol "add"   :-: args)) = add                   =<< traverse (asInteger <=< evalTerm) args
+evalTerm (List xs)                        = badAppplication       =<< traverse evalTerm xs
 evalTerm expr = return expr
 
-asInteger :: MonadError EvalError m => Term -> m Integer
-asInteger (Number n) = return n
+-- execEval :: String -> Result (Either EvalError Term)
+-- execEval str = runExcept . evalTerm <$> parse str
+
+newtype LispM env a = LispM { unLispM :: StateT env (Except EvalError) a}
+  deriving (Functor, Applicative, Monad, MonadState env, MonadError EvalError)
+  
+runLispM :: env -> LispM env a -> Either EvalError a
+runLispM env = runExcept . flip evalStateT env . unLispM 
+
+parseAndEval :: String -> LispM Env Term
+parseAndEval str = do
+  let p = parse str
+  case p of
+    Success terms -> evalTerm terms
+    _ -> throwError IllFormedSyntax
+
+main' :: String -> Either EvalError Term
+main' str = do
+  let env = Env []
+  runLispM env (parseAndEval str)
+
+
+--- | Error Handling
+
+asInteger :: MonadError EvalError m => Term -> m Term
+asInteger (Number n) = return $ Number n
+asInteger Nil = return Nil
 asInteger term = throwError $ TypeError "asInteger" term
 
 badAppplication :: MonadError EvalError m => DotList Term -> m Term
+badAppplication (x :-. xs) = throwError $ ObjectNotApplicable x
 badAppplication (x :-: xs) = throwError $ ObjectNotApplicable x
 
-quotePredicates :: MonadError EvalError m => Term -> m Term
-quotePredicates (List (p :-: e :-: Nil)) = do
+quotePredicates :: (MonadState env m, MonadError EvalError m) => Term -> m Term
+quotePredicates (List (p :-: e :-. Nil)) = do
   p' <- evalTerm p
-  return $ List (Symbol "quote" :-: (List (p' :-: (e :-: Nil)) :-: Nil))
-quotePredicates _      = throwError IllFormedSyntax
+  return $ List (Symbol "quote" :-: (List (p' :-: (e :-. Nil)) :-. Nil))
+quotePredicates Nil = return Nil
+quotePredicates x = throwError IllFormedSyntax
 
-cond :: MonadError EvalError m => DotList Term -> m Term
-cond Nil = throwError UnspecifiedReturn
+unary :: DotList Term -> Arrity
+unary (x :-: _) = Unary x
+unary (x :-. _) = Unary x
+
+binary :: DotList Term -> Arrity
+binary (x :-: y :-: _) = Binary x y
+binary (x :-: y :-. _) = Binary x y
+binary (x :-. y)       = Binary x y
+  
+arrity :: MonadError EvalError m => Int -> String -> (m Arrity -> m Term) -> DotList Term -> m Term
+arrity i name cont xs =
+  if length xs /= i + 1
+  then throwError $ WrongArrity name i (length xs - 1)
+  else case i of
+    1 -> cont . pure $ unary xs
+    2 -> cont . pure $ binary xs
+
+--- | Arithmetic
+
+add :: MonadError EvalError m => DotList Term -> m Term
+add terms = return . Number $ f terms
+  where f (Number x :-. Nil) = x
+        f (Number x :-: xs)  = x + f xs
+        f _                = 0
+
+subtract :: MonadError EvalError m => DotList Term -> m Term
+subtract terms = return . Number $ f terms
+  where f = undefined
+
+multiply :: MonadError EvalError m => DotList Term -> m Term
+multiply terms = return . Number $ f terms
+  where f = undefined
+
+divide :: MonadError EvalError m => DotList Term -> m Term
+divide terms = return . Number $ f terms
+  where f = undefined
+
+abs :: MonadError EvalError m => DotList Term -> m Term
+abs terms = return . Number $ f terms
+  where f = undefined
+
+modulo :: MonadError EvalError m => DotList Term -> m Term
+modulo terms = return . Number $ f terms
+  where f = undefined
+
+negate :: MonadError EvalError m => DotList Term -> m Term
+negate terms = return . Number $ f terms
+  where f = undefined
+
+signum :: MonadError EvalError m => DotList Term -> m Term
+signum terms = return . Number $ f terms
+  where f = undefined
+
+--- | Logic
+and :: MonadError EvalError m => DotList Term -> m Term
+and terms = return . Boolean $ f terms
+  where f = undefined
+
+
+or :: MonadError EvalError m => DotList Term -> m Term
+or terms = return . Boolean $ f terms
+  where f = undefined
+
+any :: MonadError EvalError m => DotList Term -> m Term
+any terms = return . Boolean $ f terms
+  where f = undefined
+
+all :: MonadError EvalError m => DotList Term -> m Term
+all terms = return . Boolean $ f terms
+  where f = undefined
+
+greater :: MonadError EvalError m => DotList Term -> m Term
+greater terms = return . Boolean $ f terms
+  where f = undefined
+
+less :: MonadError EvalError m => DotList Term -> m Term
+less terms = return . Boolean $ f terms
+  where f = undefined
+
+--- | McCarthy Primitives
+
+cond :: (MonadState env m, MonadError EvalError m) => DotList Term -> m Term
+cond (x :-. Nil) =
+  case x of
+    List (Boolean p  :-: e :-. Nil) -> if p then evalTerm e else throwError UnspecifiedReturn
+    Nil -> throwError UnspecifiedReturn
 cond (x :-: xs) =
   case x of
-    List (Boolean pe :-: Nil)       -> if pe then return (Boolean pe) else cond xs
-    List (Boolean p  :-: e :-: Nil) -> if p then return e else cond xs
+    List (Boolean pe :-. Nil)       -> if pe then return (Boolean pe) else cond xs
+    List (Boolean p  :-: e :-. Nil) -> if p then evalTerm e else cond xs
     List _ -> throwError IllFormedSyntax
-    DotList (Boolean p  :-: e :-: Nil) -> if p then return e else cond xs
-    DotList (Boolean pe :-: Nil)       -> if pe then return (Boolean pe) else cond xs
+    DotList (Boolean p  :-: e :-. Nil) -> if p then evalTerm e else cond xs
+    DotList (Boolean pe :-. Nil)       -> if pe then return (Boolean pe) else cond xs
     _ -> throwError IllFormedSyntax
+cond _ = throwError IllFormedSyntax
 
 eq :: MonadError EvalError m => m Arrity -> m Term
 eq terms = terms >>= \case
@@ -197,33 +325,16 @@ cdr term = term >>= \case
     Unary (List (x :-: xs))   -> return . List $ xs
     Unary term                -> throwError $ TypeError "cdr" term
 
-unary :: DotList Term -> Arrity
-unary (x :-: _) = Unary x
-unary (x :-. _) = Unary x
-
-binary :: DotList Term -> Arrity
-binary (x :-: y :-: _) = Binary x y
-binary (x :-: y :-. _) = Binary x y
-binary (x :-. y)       = Binary x y
-  
-arrity :: MonadError EvalError m => Int -> String -> (m Arrity -> m Term) -> DotList Term -> m Term
-arrity i name cont xs =
-  if length xs /= i
-  then throwError $ WrongArrity name i (length xs)
-  else case i of
-    1 -> cont . pure $ unary xs
-    2 -> cont . pure $ binary xs
-    _ -> cont . pure $ Nary xs
-
 atom :: MonadError EvalError m => m Arrity -> m Term
 atom mterm = mterm >>= \case
-  Unary (DotList _) -> return $ Boolean False
-  Unary (List _)    -> return $ Boolean False
-  Unary _           -> return $ Boolean True
+  Unary (DotList xs) -> throwError $ NotAProperList (DotList xs)
+  Unary (List _)     -> return     $ Boolean False
+  Unary _            -> return     $ Boolean True
 
-execEval :: String -> Result (Either EvalError Term)
-execEval str = runExcept . evalTerm <$> parse str
-
+lambda :: MonadError EvalError m => m Arrity -> m Term
+lambda mterm = mterm >>= \case
+  Binary args body -> undefined
+  
 
 --------------
 ---- REPL ----
@@ -238,12 +349,13 @@ prompt text = do
   getLine
 
 repl :: Repl ()
-repl = forever $ do
-  rawInput <- getInputLine "> "
-  case rawInput of
-    Nothing -> repl
-    Just input -> do
-       let parsedResult = parse input
-       let evalResult = execEval input
-       liftIO $ print evalResult
+repl = undefined
+-- repl = forever $ do
+--   rawInput <- getInputLine "> "
+--   case rawInput of
+--     Nothing -> repl
+--     Just input -> do
+--        let parsedResult = parse input
+--        let evalResult = execEval input
+--        liftIO $ print evalResult
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -44,20 +44,20 @@ checkParse = describe "Test Parser" $
         , ("False", Boolean False)
         , ("\"Foobar\"", String "Foobar")
         , ("add", Symbol "add")
-        , ("()", List Nil)
-        , ("(1)", List (Number 1 :-: Nil))
-        , ("(True)", List (Boolean True :-: Nil))
-        , ("(1 2 3)", List (Number 1 :-: Number 2 :-: Number 3 :-: Nil))
-        , ("(1 2 (True False))", List (Number 1 :-: Number 2 :-: List (Boolean True :-: Boolean False :-: Nil) :-: Nil))
+        , ("()", Nil)
+        , ("(1)", List (Number 1 :-. Nil))
+        , ("(True)", List (Boolean True :-. Nil))
+        , ("(1 2 3)", List (Number 1 :-: Number 2 :-: Number 3 :-. Nil))
+        , ("(1 2 (True False))", List (Number 1 :-: Number 2 :-: List (Boolean True :-: Boolean False :-. Nil) :-. Nil))
         , ("(1 . 2)", DotList (Number 1 :-. Number 2))
         , ("(1 2 . 3)", DotList (Number 1 :-: Number 2 :-. Number 3))
         , ("(1 . (2 . (3 . 4)))", DotList (Number 1 :-. DotList (Number 2 :-. DotList (Number 3 :-. Number 4))))
-        , ("'(1 2)", List (Symbol "quote" :-: List (Number 1 :-: Number 2 :-: Nil) :-: Nil))
-        , ("(add 1 2)", List (Symbol "add" :-: Number 1 :-: Number 2 :-: Nil))
-        , ("(lambda x (add 1 2))", List (Symbol "lambda" :-: Symbol "x" :-: List (Symbol "add" :-: Number 1 :-: Number 2 :-: Nil) :-: Nil))
-        , ("(cons 1 2)", List (Symbol "cons" :-: Number 1 :-: Number 2 :-: Nil))
-        , ("(cons 1 (cons 2 ()))", List (Symbol "cons" :-: Number 1 :-: List (Symbol "cons" :-: Number 2 :-: (List Nil :-: Nil)) :-: Nil))
-        , ("(cons 1 '(2 3))", List (Symbol "cons" :-: Number 1 :-: List (Symbol "quote" :-: List (Number 2 :-: Number 3 :-: Nil) :-: Nil) :-: Nil))
+        , ("'(1 2)", List (Symbol "quote" :-: List (Number 1 :-: Number 2 :-. Nil) :-. Nil))
+        , ("(add 1 2)", List (Symbol "add" :-: Number 1 :-: Number 2 :-. Nil))
+        , ("(lambda x (add 1 2))", List (Symbol "lambda" :-: Symbol "x" :-: List (Symbol "add" :-: Number 1 :-: Number 2 :-. Nil) :-. Nil))
+        , ("(cons 1 2)", List (Symbol "cons" :-: Number 1 :-: Number 2 :-. Nil))
+        , ("(cons 1 (cons 2 ()))", List (Symbol "cons" :-: Number 1 :-: List (Symbol "cons" :-: Number 2 :-: (Nil :-. Nil)) :-. Nil))
+        , ("(cons 1 '(2 3))", List (Symbol "cons" :-: Number 1 :-: List (Symbol "quote" :-: List (Number 2 :-: Number 3 :-. Nil) :-. Nil) :-. Nil))
         ]
    
 -- | TODO: Add unhappy evaluations
@@ -72,49 +72,49 @@ checkEval = describe "Test Evaluation" $ do
       , (String "Foobar", String "Foobar")
       , (Symbol "add", Symbol "add")
       , (Symbol "lambda", Symbol "lambda")
-      , (List Nil, List Nil)
+      , (Nil, Nil)
 
       -- add
-      , (List (Symbol "add" :-: Number 1 :-: Number 2 :-: Nil), Number 3)
-      , (List (Symbol "add" :-: Number 1 :-: List (Symbol "add" :-: Number 2 :-: List (Symbol "add" :-: Number 3 :-: Number 4 :-: Nil) :-: Nil) :-: Nil), Number 10)
+      , (List (Symbol "add" :-: Number 1 :-: Number 2 :-. Nil), Number 3)
+      , (List (Symbol "add" :-: Number 1 :-: List (Symbol "add" :-: Number 2 :-: List (Symbol "add" :-: Number 3 :-: Number 4 :-. Nil) :-. Nil) :-. Nil), Number 10)
       -- quote
-      , (List (Symbol "quote" :-: List (Number 1 :-: Number 2 :-: Nil) :-: Nil), List (Number 1 :-: Number 2 :-: Nil))
-      , (List (Symbol "quote" :-: DotList (Number 1 :-. Number 2) :-: Nil), DotList (Number 1 :-. Number 2))
+      , (List (Symbol "quote" :-: List (Number 1 :-: Number 2 :-. Nil) :-. Nil), List (Number 1 :-: Number 2 :-. Nil))
+      , (List (Symbol "quote" :-: DotList (Number 1 :-. Number 2) :-. Nil), DotList (Number 1 :-. Number 2))
       -- atom
-      , (List (Symbol "atom?" :-: Number 1 :-: Nil), Boolean True)
-      , (List (Symbol "atom?" :-: DotList Nil :-: Nil), Boolean False)
-      , (List (Symbol "atom?" :-: List (Symbol "quote" :-: List (Number 1 :-: Nil) :-: Nil) :-: Nil) , Boolean False)
+      , (List (Symbol "atom?" :-: Number 1 :-. Nil), Boolean True)
+      , (List (Symbol "atom?" :-: List (Symbol "quote" :-: List (Number 1 :-. Nil) :-. Nil) :-. Nil) , Boolean False)
       -- eq
-      , (List (Symbol "eq?" :-: Number 1 :-: Number 1 :-: Nil), Boolean True)
-      , (List (Symbol "eq?" :-: Number 1 :-: Number 2 :-: Nil), Boolean False)
-      , (List (Symbol "eq?" :-: Number 3 :-: List (Symbol "add" :-: Number 1 :-: Number 2 :-: Nil) :-: Nil), Boolean True)
+      , (List (Symbol "eq?" :-: Number 1 :-: Number 1 :-. Nil), Boolean True)
+      , (List (Symbol "eq?" :-: Number 1 :-: Number 2 :-. Nil), Boolean False)
+      , (List (Symbol "eq?" :-: Number 3 :-: List (Symbol "add" :-: Number 1 :-: Number 2 :-. Nil) :-. Nil), Boolean True)
       -- cons
-      , (List (Symbol "cons" :-: Number 1 :-: List (Symbol "quote" :-: List (Number 2 :-: Number 3 :-: Nil) :-: Nil) :-: Nil), List (Number 1 :-: Number 2 :-: Number 3 :-: Nil))
-      , (List (Symbol "cons" :-: Number 1 :-: Number 2 :-: Nil), DotList (Number 1 :-. Number 2))
+      , (List (Symbol "cons" :-: Number 1 :-: List (Symbol "quote" :-: List (Number 2 :-: Number 3 :-. Nil) :-. Nil) :-. Nil), List (Number 1 :-: Number 2 :-: Number 3 :-. Nil))
+      , (List (Symbol "cons" :-: Number 1 :-: Number 2 :-. Nil), DotList (Number 1 :-. Number 2))
       -- car
-      , (List (Symbol "car" :-: List (Symbol "quote" :-: List (Number 1 :-: Number 2 :-: Nil) :-: Nil) :-: Nil), Number 1)
-      , (List (Symbol "car" :-: List (Symbol "quote" :-: DotList (Number 1 :-. Number 2) :-: Nil) :-: Nil), Number 1)
+      , (List (Symbol "car" :-: List (Symbol "quote" :-: List (Number 1 :-: Number 2 :-. Nil) :-. Nil) :-. Nil), Number 1)
+      , (List (Symbol "car" :-: List (Symbol "quote" :-: DotList (Number 1 :-. Number 2) :-. Nil) :-. Nil), Number 1)
       -- cdr
-      , (List (Symbol "cdr" :-: List (Symbol "quote" :-: List (Number 1 :-: Number 2 :-: Nil) :-: Nil) :-: Nil), List (Number 2 :-: Nil))
+      , (List (Symbol "cdr" :-: List (Symbol "quote" :-: List (Number 1 :-: Number 2 :-. Nil) :-. Nil) :-. Nil), List (Number 2 :-. Nil))
       --, (List (Symbol "cdr" :-: List (Symbol "quote" :-: DotList (Number 1 :-. Number 2) :-: Nil) :-: Nil), Number 2)
       ]
   describe "Failure" $
     mapM_ (uncurry specEvalYields) $
       getCompose $ Left <$> Compose
-      [ (List (Number 1 :-: Nil), ObjectNotApplicable (Number 1))
+      [ (List (Number 1 :-. Nil), ObjectNotApplicable (Number 1))
       -- add -- TODO: figure out how to label the error thrown from `asInteger` with the actual func called
-      , (List (Symbol "add" :-: Number 1 :-: Boolean True :-: Nil), TypeError "asInteger" (Boolean True))
+      , (List (Symbol "add" :-: Number 1 :-: Boolean True :-. Nil), TypeError "asInteger" (Boolean True))
       -- quote
       -- atom
+      , (List (Symbol "atom?" :-: DotList (Nil :-. Nil) :-. Nil), NotAProperList (DotList (Nil :-. Nil)))
       -- eq
       -- cons
-      , (List (Symbol "cons" :-: List (Number 1 :-: Number 2 :-: Nil) :-: Number 3 :-: Nil), ObjectNotApplicable (Number 1))
+      , (List (Symbol "cons" :-: List (Number 1 :-: Number 2 :-. Nil) :-: Number 3 :-. Nil), ObjectNotApplicable (Number 1))
       -- car
-      , (List (Symbol "car" :-: List (Number 1 :-: Number 2 :-: Nil) :-: Nil), ObjectNotApplicable (Number 1))
+      , (List (Symbol "car" :-: List (Number 1 :-: Number 2 :-. Nil) :-. Nil), ObjectNotApplicable (Number 1))
       -- cdr
-      , (List (Symbol "cdr" :-: List (Number 1 :-: Number 2 :-: Nil) :-: Nil), ObjectNotApplicable (Number 1))
+      , (List (Symbol "cdr" :-: List (Number 1 :-: Number 2 :-. Nil) :-. Nil), ObjectNotApplicable (Number 1))
       -- List
-      , (List (Boolean True :-: Number 1 :-: Nil), ObjectNotApplicable (Boolean True))
+      , (List (Boolean True :-: Number 1 :-. Nil), ObjectNotApplicable (Boolean True))
       ]
 
 main :: IO ()
